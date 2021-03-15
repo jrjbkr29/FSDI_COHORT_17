f01cd10445b041af2dc1d74b6dc6eec46453cb18
jrjbkr29 <78834535+jrjbkr29@users.noreply.github.com>
2021-03-14 21:09:44 -0700

    Add files via upload

--- 104/Competency_Report/js/display.js
+++ 104/Competency_Report/js/display.js
@@ -1,34 +1,38 @@
 function display(aPet){
-    document.getElementById("pets").innerHTML = "";
-    //for (var i=0;i<salon.pets.length;i++){
-       // var aPet = salon.pets[i];
-        var tmp =  `
-        <div class="pet"> 
-            <h3>${aPet.name}</h3>
-            <p> Age: ${aPet.age}</p>
-            <p> Gender ${aPet.gender}</p>
-            <p> Breed ${aPet.breed}</p>
-            <p> Service ${aPet.service}</p>
-            <p> Owner ${aPet.owner}</p>
-            <p> Phone ${aPet.number}</p>
-        </div>
-        `;
-        document.getElementById("pets").innerHTML+=tmp;
-    //}
+        document.getElementById("pets").innerHTML = "";
+            for (var i=0;i<salon.pets.length;i++){
+            var aPet = salon.pets[i];
+                var tmp =  `
+                <div class="pet"> 
+                    <h3>${aPet.name}</h3>
+                    <p> Age: ${aPet.age}</p>
+                    <p> Gender: ${aPet.gender}</p>
+                    <p> Breed: ${aPet.breed}</p>
+                    <p> Service: ${aPet.service}</p>
+                    <p> Owner: ${aPet.owner}</p>
+                    <p> Phone: ${aPet.number}</p>
+                    <p> Payment Method: ${aPet.pay}</p>
+                </div>
+                `;
+                document.getElementById("pets").innerHTML+=tmp;
+            }
 }
-//display();
 
 function displayTable(aPet){
-    var row=`
-    <tr>
-        <td>${aPet.name}</td>
-        <td>${aPet.age}</td>
-        <td>${aPet.gender}</td>
-        <td>${aPet.breed}</td>
-        <td>${aPet.service}</td>
-        <td>${aPet.owner}</td>
-        <td>${aPet.phone}</td>
-    </tr>    
-    `;
-    document.getElementById('pet-table').innerHTML+=row;
+    for (var i=0;i<salon.pets.length;i++){
+        var aPet = salon.pets[i];
+        var tmp=`
+            <tr id="pet-table-info">
+                <td class="pet-table-cell">${aPet.name}</td>
+                <td class="pet-table-cell">${aPet.age}</td>
+                <td class="pet-table-cell">${aPet.gender}</td>
+                <td class="pet-table-cell">${aPet.breed}</td>
+                <td class="pet-table-cell">${aPet.service}</td>
+                <td class="pet-table-cell">${aPet.owner}</td>
+                <td class="pet-table-cell">${aPet.phone}</td>
+                <td class="pet-table-cell">${aPet.pay}</td>
+            </tr>    
+            `;
+            document.getElementById('pet-table').innerHTML+=tmp;
+    }
 }
