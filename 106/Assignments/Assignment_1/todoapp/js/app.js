499aee1d224a3f9189f32352999082e5df17b3f3
jrjbkr29 <78834535+jrjbkr29@users.noreply.github.com>
2021-03-30 19:42:11 -0700

    Add files via upload

---
+++ 106/Assignments/Assignment_1/todoapp/js/app.js
@@ -0,0 +1,27 @@
+function saveToDo() {
+    console.log("Clicked!")
+    // get the text from the input field
+    var todo = $('#todotext').val();
+    // create DOM element
+    var syntax = "<li class=list-group-item>" + todo + "</li>";
+    $('#pendingTodos').append(syntax);
+
+    // clear the text
+    $('#todotext').val(" ").focus();
+}
+
+function init() {
+    console.log("Todo App");
+
+    // hook events
+    $('#btnSave').click(saveToDo);
+    $('#todotext').keypress(function (e) {
+        if (e.key === "Enter") {
+            saveToDo();
+        }
+    });
+}
+
+
+
+window.onload = init;
