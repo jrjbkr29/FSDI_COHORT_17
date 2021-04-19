f4f072fd0c6f72fd70e1ba38ef119816bf03a7ef
jrjbkr29 <78834535+jrjbkr29@users.noreply.github.com>
2021-04-18 22:37:38 -0700

    Add files via upload

---
+++ 107/taskManager/Models/Contact.cs
@@ -0,0 +1,12 @@
+namespace taskManager.Models
+{
+    public class Contact
+    {
+        public int ID {get; set;}
+        public string Name {get;set;}
+        
+        public string Email {get;set;}
+
+        public string Message {get;set;}
+    }
+}
---
+++ 107/taskManager/Models/Task.cs
@@ -0,0 +1,20 @@
+namespace taskManager.Models
+{
+    public class Task
+    {
+        public int ID {get; set;}
+        public string Title {get;set;}
+        
+        public string Description {get;set;}
+
+        public bool Important {get;set;}
+
+        public string DueDate {get;set;}
+
+        public string Location {get;set;}
+
+        public string AlertText {get;set;}
+
+        public int Status {get;set;}
+    }
+}
